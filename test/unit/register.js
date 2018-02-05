06e1c5d82761b6ce75e616bd6e228503618b1228
sergioalexandermendietaarias <sergioalexandermendietaarias@25.60.245.154>
2018-02-05 06:51:55 -0500

    cambios en adaptador

--- test/unit/register.js
+++ test/unit/register.js
@@ -2,18 +2,17 @@
  * Test dependencies
  */
 var Adapter = require('../../');
-console.log("register file");
-console.log(Adapter);
-
-//Adapter.registerCollection();
+var mocha = require('mocha');
+const { describe, it, before, after } = require('mocha');
 
 describe('registerCollection', function () {
 	console.log('describe method it has bean called for the registerCollection');
 	
 	it('should not hang or encounter any errors', function (done) {
 		Adapter.registerCollection({
-			identity: 'foo'
-		}, done);
+			identity: 'foo',
+			projectId: 'quiverentregasrapidas'
+		}, {},done);
 	});	
 	
 });
